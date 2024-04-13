41996a14bd86010893f9f278c3ffd9105de4d876
Mircea Dobreanu <mircea@dobreanu.net>
2024-04-13 12:52:31 +0300

    Create scaffolding for Assets CRUD operations

--- assetassistant-api/main.go
+++ assetassistant-api/main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
-//    "net/http"
-    "github.com/gin-gonic/gin"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-    router := gin.Default()
+	router := gin.Default()
+	router.SetTrustedProxies(nil)
 
-    router.Run("localhost:8080")
+	apiRoutesV1 := router.Group("/api/v1")
+	assetsResources := apiRoutesV1.Group("/assets")
+
+	assetsResources.GET("", findAssets)
+	assetsResources.GET("/:asset-id", findAsset)
+
+	assetsResources.POST("", createAsset)
+	assetsResources.PUT("/:asset-id", replaceAsset)
+
+	assetsResources.DELETE("/:asset-id", deleteAsset)
+
+	router.Run("localhost:8080")
+}
+
+func findAssets(context *gin.Context) {
+	context.JSON(http.StatusOK, nil)
+}
+
+func findAsset(context *gin.Context) {
+	panic(errors.ErrUnsupported.Error())
+}
+
+func createAsset(context *gin.Context) {
+	panic(errors.ErrUnsupported.Error())
+}
+
+func replaceAsset(context *gin.Context) {
+	panic(errors.ErrUnsupported.Error())
+}
+
+func deleteAsset(context *gin.Context) {
+	panic(errors.ErrUnsupported.Error())
 }
