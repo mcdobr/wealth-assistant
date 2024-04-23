1648188d05ed7d4129b30e1b5a6d58d12976f81a
Mircea Dobreanu <mircea@dobreanu.net>
2024-04-23 21:34:34 +0300

    Implement basic login/logout with Auth0

--- assetassistant-web/src/App.jsx
+++ assetassistant-web/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import CompoundInterestCalculator from './CompoundInterestCalculator.jsx'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
   return (
     <>
       <h1>AssetAssistant Web</h1>
+
+      <LoginButton />
+      <LogoutButton />
+      {/*
       <button onClick={event => { handleSubmit(event) }}>Click me</button>
 
       <CompoundInterestCalculator/>
+      */}
     </>
   )
 }
@@ -17,6 +23,28 @@ const handleSubmit = (e) => {
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
+}
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+  
+  return (
+    <>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+    </>
+  );
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <>
+      <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+        Log Out
+      </button>
+    </>
+  );
 }
 
 export default App
