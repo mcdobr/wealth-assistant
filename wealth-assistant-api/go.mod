d7763575130ec106336d0020671a95dd641ce6df
Mircea Dobreanu <mircea@dobreanu.net>
2025-03-02 02:36:07 +0200

    Add gohateoas dependency

--- wealth-assistant-api/go.mod
+++ wealth-assistant-api/go.mod
@@ -3,13 +3,17 @@ module assetassistant-api
 go 1.22.1
 
 require (
+	github.com/gin-contrib/cors v1.7.1
+	github.com/gin-gonic/gin v1.9.1
+	github.com/ing-bank/gohateoas v1.1.0
+)
+
+require (
 	github.com/bytedance/sonic v1.11.3 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/gin-contrib/cors v1.7.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
@@ -21,6 +25,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.1 // indirect
+	github.com/survivorbat/go-tsyncmap v0.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.7.0 // indirect
--- wealth-assistant-api/main.go
+++ wealth-assistant-api/main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/ing-bank/gohateoas"
 )
+
+type ApiResponse struct {
+	Data any `json:"data"`
+}
+
+type Asset struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+type Income struct {
+	ID int64
+}
+
+type Expense struct {
+	ID int64
+}
+
+func (apiResponse ApiResponse) MarshalJson() ([]byte, error) {
+	return json.Marshal(struct {
+		Data json.RawMessage `json:"data"`
+	}{
+		Data: gohateoas.InjectLinks(gohateoas.DefaultLinkRegistry, apiResponse.Data),
+	})
+}
 
 func main() {
 	router := gin.Default()
@@ -20,6 +47,8 @@ func main() {
 			ExposeHeaders:    []string{"Content-Length"},
 			AllowCredentials: true,
 		}))
+
+	gohateoas.Register(Asset{}, gohateoas.Self("/api/v1/assests/{asset-id}", "Get the asset"))
 
 	apiRoutesV1 := router.Group("/api/v1")
 	assetsResources := apiRoutesV1.Group("/assets")
@@ -40,7 +69,15 @@ func findAssets(context *gin.Context) {
 }
 
 func findAsset(context *gin.Context) {
-	panic(errors.ErrUnsupported.Error())
+	// For now, we can imagine that a DB lookup happens here
+	asset := Asset{
+		ID:   1,
+		Name: "S&P 500 Index",
+	}
+
+	context.JSON(http.StatusOK, ApiResponse{
+		Data: asset,
+	})
 }
 
 func createAsset(context *gin.Context) {
